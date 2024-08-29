e7f800e641dfcd4abfcbb4513495b49e50fbfa3a
mshakeer03 <96262163+mshakeer03@users.noreply.github.com>
2024-08-29 18:15:09 +0530

    Update icebreaker.py

--- icebreaker.py
+++ icebreaker.py
@@ -5,12 +5,11 @@ import os
 from dotenv import load_dotenv
 information= """
     Avul Pakir Jainulabdeen Abdul Kalam BR (Abdul Kalam, born 15 October, 1931 died 27 July 2015) was an Indian aerospace 
-    scientist and statesman who served as the 11th president of India from 2002 to 2007. 
-    Born and raised in a Muslim family in Rameswaram, Tamil Nadu, he studied physics and aerospace engineering. 
-    He spent the next four decades as a scientist and science administrator, mainly at the Defence Research and Development 
-    Organisation (DRDO) and Indian Space Research Organisation (ISRO) and was intimately involved in India's 
-    civilian space programme and military missile development efforts.[1] He thus came to be known as the Missile Man of India 
-    for his work on the development of ballistic missile and launch vehicle technology.
+    scientist and statesman who served as the 11th president of India from 2002 to 2007. Born and raised Rameswaram, Tamil Nadu, 
+    he studied physics and aerospace engineering. He spent the next four decades as a scientist and science administrator, 
+    mainly at the Defence Research and Development Organisation (DRDO) and Indian Space Research Organisation (ISRO) and was 
+    intimately involved in India's civilian space programme and military missile development efforts.[1] He thus came to 
+    be known as the Missile Man of India for his work on the development of ballistic missile and launch vehicle technology.
     """
 if __name__ == '__main__':
     # load_dotenv()
@@ -25,8 +24,8 @@ if __name__ == '__main__':
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model="llama3")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOllama(model="llama3")
     chain = summary_prompt_template | llm 
     res = chain.invoke(input={"information": information})
     print(res)
